e0b95f9beefc4a0e3e970178e53ad0f0a0a655f2
sjahed7 <sjahed7@uw.edu>
2022-10-10 16:07:00 -0700

    lab stuff

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # lab2testing
+# lab 2 demo
+
+## welcome to lab 2
+
+- fruit
+- vegetables
+- drinks
+- candy
+- icecream
+
+1. INFO 200
+2. INFO 402
+3. INFO 201
---
+++ testingscript.R
@@ -0,0 +1,13 @@
+# creating variables
+# no spaces, don't start with numbers
+my_var <- 10
+
+# ?(function()) will show the function description and parameters
+
+#funtion 
+# create a function that will return the cubed result of given number x
+
+cubed <- function(x) {
+  result_cubed <- x^3
+  return(result_cubed)
+}
